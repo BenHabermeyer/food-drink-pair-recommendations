aacea4d1e01e31459926857b61b32e040ab14a35
anitasyang <51406060+anitasyang@users.noreply.github.com>
2020-04-09 14:41:19 +1200

    Update db-config.js

--- server/db-config.js
+++ server/db-config.js
@@ -1,7 +1,7 @@
 // Private. Will not be included in submission
 module.exports = {
-  host: "fling.seas.upenn.edu",
-  user: "<username>",
-  password: "<password>",
-  database: "<username>"
+  host: "cis550foodwinebeer.cz9cryizahld.us-east-1.rds.amazonaws.com",
+  user: "PennDrinkers",
+  password: "Quaker2020",
+  database: "CIS550FoodWineBeer"
 };
