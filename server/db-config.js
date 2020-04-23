9f3bc2c2ad7b46426c38a20587a8d8f66a2a119c
anitasyang <51406060+anitasyang@users.noreply.github.com>
2020-04-23 21:20:53 +1200

    Delete db-config.js

--- server/db-config.js
+++
@@ -1,7 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "cis550foodwinebeer.cz9cryizahld.us-east-1.rds.amazonaws.com",
-  user: "PennDrinkers",
-  password: "Quaker2020",
-  database: "CIS550FoodWineBeer"
-};
