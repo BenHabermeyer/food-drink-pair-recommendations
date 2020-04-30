351a994713ad3ae6d1b0fd83e7421365ccb4c0b8
anitayang <asuryani2151@gmail.com>
2020-05-01 11:55:59 +1200

    changed font again

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -68,14 +68,14 @@ export default class Home extends React.Component {
         <PageNavbar active="home" />
 
         <br></br>
-        <div className="container movies-container">
-          <div className="title">Welcome to Food-Drink Pairer!</div>
+        <div className="container home-container">
+          <div className="headerHome">Welcome to Food-Drink Pairer!</div>
           <div className="text-container">
             <div className="text">This application is a one-stop-shop for you to find the perfect pairing for your individual needs. With information on beer types and quality, wine types and quality, breweries, wineries, price, food pairings and their ratings, this application will allow the user to search for the perfect accompaniment for their meal. 
             </div>
           </div>
           <br></br>
-          <div className="title"><strong>Info</strong></div>
+          <div className="title">Info</div>
           <div className="text-container">
             <div className="text"> Get to know where your wine/beer comes from!</div>
           </div>
@@ -83,7 +83,7 @@ export default class Home extends React.Component {
           <img className="resize" src={winery} alt="winery"/>
           
           <br></br>
-          <div className="title"><strong>Pair</strong></div>
+          <div className="title">Pair</div>
           <div className="text-container">
             <div className="text"> Find out what the best food are for your drink and what the best drink are for your food for the optimal dining experience!
             </div>
@@ -91,7 +91,7 @@ export default class Home extends React.Component {
           <br></br>
           <img className="resize" src={food_wine} alt="food and wine"/>
           <br></br>
-          <div className="title"><strong>Map</strong></div>
+          <div className="title">Map</div>
           <div className="text-container">
             <div className="text"> Find out where the different wineries and breweries are located in the US and try out their best offering!</div>
           </div>
